2a3154ca5a7d577624dfc9f8c9da5dd9c8e4e207
pragadeeshkn <pragadeeshkn2007@gmail.com>
2026-02-27 16:00:03 +0530

    UC3: hardcoded palindrome

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -33,3 +33,28 @@ public class PalindromeCheckerApp {
             }
         }
     }
+
+
+
+
+
+import java.util.Scanner;
+
+public class UseCase3PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String original = scanner.nextLine();
+        String reversed = "";
+        for (int i = original.length() - 1; i >= 0; i--) {
+            reversed += original.charAt(i);
+        }
+        if (original.equals(reversed)) {
+            System.out.println("Result: The string is a Palindrome.");
+        } else {
+            System.out.println("Result: The string is NOT a Palindrome.");
+        }
+        scanner.close();
+    }
+}
